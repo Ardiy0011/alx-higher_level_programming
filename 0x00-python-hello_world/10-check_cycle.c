1f70859cb6b8422f332e7fefd5fd819ec2a1a221
Ardiy <you@example.com>
2023-06-06 01:35:37 -0700

    draft

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,27 +2,25 @@
 
 /**
  * check_cycle - checks if a linked list contains a cycle
- * @list: head linked list in traversal
- * Return: 1 if last 2 nodes before the Null corrolate in
- * several instances, 0 if it doesn't
+ * @list: linked list to check
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
  */
-
-
-
 int check_cycle(listint_t *list)
 {
-    listint_t *sloth, *the_flash;
-    sloth, the_flash = list;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-    while (the_flash != NULL && the_flash->next != NULL)
-    {
-        sloth = sloth->next;
-        the_flash = the_flash->next;
-        the_flash = the_flash->next;
+	if (!list)
+		return (0);
 
-        if (sloth == the_flash)
-            return (1);
-    }
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
 
-    return (0);
+	return (0);
 }
