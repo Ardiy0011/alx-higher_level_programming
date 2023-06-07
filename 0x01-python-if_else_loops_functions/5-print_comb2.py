e1c6f8cc819c14416c2256be279720fb1e3bef3d
Ardiy <you@example.com>
2023-06-07 02:00:04 -0700

    comb1

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for a in range(0, 100):
-    
-    print("{},".format(str(a).zfill(2)), end=' ') 
     if a == 99:
-        print("\b\b ")
+        print("{}".format(str(a).zfill(2)), end=' ')
+    else:
+        print("{},".format(str(a).zfill(2)), end=' ') 
+
   
