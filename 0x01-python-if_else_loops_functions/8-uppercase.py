8fcd2c38fad06b18af6fe235236698e8c9573a99
Ardiy <you@example.com>
2023-06-07 02:45:19 -0700

    uppercase print

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,9 +5,3 @@ def uppercase(string):
             i = chr(ord(i) - 32)
         print("{}".format(i), end='')
     print()
-
-
-uppercase = __import__('8-uppercase').uppercase
-
-uppercase("best")
-uppercase("Best School 98 Battery street")
