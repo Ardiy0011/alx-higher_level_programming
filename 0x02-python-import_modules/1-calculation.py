bd35233f96cf324e38f3601bef7d74bd8883aaf0
Ardiy <you@example.com>
2023-06-08 21:09:09 +0000

    general match imports

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import calculator_1 as calc
+
+a = 10
+b = 5
+
+print("{} + {} = {}".format(a, b, (calc.add(a, b))))
+print("{} - {} = {}".format(a, b, (calc.subtract(a, b))))
+print("{} * {} = {}".format(a, b, (calc.multiply(a, b))))
+print("{} / {} = {}".format(a, b, int((calc.divide(a, b)))))
