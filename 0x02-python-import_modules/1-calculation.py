3ea22907b182d9037c652d939e33ea3d5edd620e
Ardiy <you@example.com>
2023-06-08 23:01:44 +0000

    general match imports

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, subtract(a, b)))
     print("{} * {} = {}".format(a, b, multiply(a, b)))
-    print("{} / {} = {}".format(a, b, int(divide(a, b))), end='')
+    print("{:d} / {:d} = {:d}".format(a, b, int(divide(a, b))), end='')
