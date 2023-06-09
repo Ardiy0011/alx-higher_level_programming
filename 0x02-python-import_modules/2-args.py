442d9cd42c4d62e90c2b9a20382fe6bfc6c6107d
Ardiy <you@example.com>
2023-06-09 00:06:28 +0000

    argv length

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+def arglist(argv):
+    length = len(argv) - 1
+    if length == 0:
+        print("{} argument.".format(length))
+    elif length == 1:
+        print("{} argument:".format(length))
+    else:
+        print("{} arguments:".format(length))
+        for i in range(1, length + 1):
+            print("{}: {}".format(i, argv[i]))
+
+if __name__ == "__main__":
+    import sys
+    arglist(sys.argv)
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,1 +1,2 @@
-#!/usr/bin/python3#!/usr/bin/python3
+#!/usr/bin/python3#!/usr/bin/python3        for i in range(length):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
