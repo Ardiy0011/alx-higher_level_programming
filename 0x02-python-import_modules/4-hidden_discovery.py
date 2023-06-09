505c50c0d79050d734608e918279426a27e4eca5
Ardiy <you@example.com>
2023-06-09 01:10:59 +0000

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+import hidden_4
+
+
+def discovr():
+    secret = dir(hidden_4)
+    for a in secret:
+        if a[:2] != '__':
+            print(a)
+
+
+if __name__ == "__main__":
+    discovr()
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3#!/usr/bin/python3        for i in range(length):
-            print("{:d}: {:s}".format(i, sys.argv[i]))
+#!/usr/bin/python3
+import variable_load_5
+
+
+def find_var():
+    print(a)
+
+
+if __name__ == "__main__":
+    find_var()
