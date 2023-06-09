fbe00255bdb9cef492859cd902c77038df0afa8c
Ardiy <you@example.com>
2023-06-09 01:56:38 +0000

    my calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
 
-import sys
 from calculator_1 import add, sub, mul, div
+import sys
 
-def main():
-    length = len(sys.argv) - 1
-    maths = {'+': add, '-': sub, '*': mul, '/': div}
+def calculate(a, operator, b):
+    if operator == '+':
+        return add(a, b)
+    elif operator == '-':
+        return sub(a, b)
+    elif operator == '*':
+        return mul(a, b)
+    elif operator == '/':
+        return div(a, b)
+    else:
+        return None
 
-    if length != 3:
+if __name__ == "__main__":
+    length = len(sys.argv)
+    if length != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    if sys.argv[2] not in maths:
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    result = calculate(a, operator, b)
+    if result is None:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    a, operator, b = map(int, sys.argv[1:])
-    result = maths[operator](a, b)
-    print("{} {} {} = {}".format(a, operator, b, result))
-
-if __name__ == "__main__":
-    main()
+    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
