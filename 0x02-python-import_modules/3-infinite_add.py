bddab5985352e8de3fb138542ece08cf03375157
Ardiy <you@example.com>
2023-06-09 00:55:38 +0000

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+def arglist(argv):
+    length = len(argv) - 1
+    total = 0
+
+
+    i = 1
+
+    while i <= length:
+        total += int(argv[i])
+        i += 1
+    print(total)
+
+
+if __name__ == "__main__":
+    import sys
+    arglist(sys.argv)
