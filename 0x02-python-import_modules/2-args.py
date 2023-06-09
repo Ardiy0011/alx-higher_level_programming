ac43207f4fb97ed3794a4843d077d12af3c12f45
Ardiy <you@example.com>
2023-06-09 00:18:43 +0000

    argv length

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,10 @@
 def arglist(argv):
     length = len(argv) - 1
     if length == 0:
-        print("{} argument.".format(length))
+        print("{} arguments.".format(length))
     elif length == 1:
         print("{} argument:".format(length))
+        print("1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(length))
         for i in range(1, length + 1):
