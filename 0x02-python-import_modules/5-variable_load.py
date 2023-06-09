91f8cb0f764a478f3cd6d6769c42050fb7aa4a65
Ardiy <you@example.com>
2023-06-09 01:22:18 +0000

    simple variable import

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python3#!/usr/bin/python3        for i in range(length):
 #!/usr/bin/python3
 import variable_load_5
 
 
-def find_var():
-    print(a)
+def find_variable():
+    print(variable_load_5.a)
 
 
 if __name__ == "__main__":
-    find_var()
+    find_variable()
