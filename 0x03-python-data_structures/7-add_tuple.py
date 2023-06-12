ec5e638bbc882e866e07d70fc0dbf671bc3caaaa
Ardiy <you@example.com>
2023-06-12 18:16:25 +0000

    print matrix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -19,13 +19,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_b) == 1:
         temp_a += tuple_b[0]
     return (temp_a, temp_b)
-
-add_tuple = __import__('7-add_tuple').add_tuple
-
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
