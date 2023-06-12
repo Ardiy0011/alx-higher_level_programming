ba63622348ce6f896f353df094790b8c23ee27d7
Ardiy <you@example.com>
2023-06-12 22:11:45 +0000

    max int

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -8,9 +8,3 @@ def max_integer(my_list=[]):
         if i > max_value:
             max_value = i
             return max_value
-
-max_integer = __import__('9-max_integer').max_integer
-
-my_list = [1, 90, 2, 13, 34, 5, -13, 3]
-max_value = max_integer(my_list)
-print("Max: {}".format(max_value))
