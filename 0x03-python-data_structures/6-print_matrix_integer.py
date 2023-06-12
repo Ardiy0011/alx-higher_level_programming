193e3374aeb9477361d6f774704cf38e903e95eb
Ardiy <you@example.com>
2023-06-12 18:09:47 +0000

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for string in matrix:
-        for element in string:
-            print("{:d}".format(element), end=' ')
-        print()
+    for i in matrix:
+        print(' '.join('{:d}'.format(j)for j in i))
--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -47,3 +47,25 @@ def main():
     print(my_list)
 
 main() """
+
+
+
+""" task 6
+
+def print_matrix_integer(matrix=[[]]):
+    for string in matrix:
+        for element in string:
+            print("{:d}".format(element), end=' ')
+        print()
+
+print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer() """
