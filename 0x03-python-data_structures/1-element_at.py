559c507daf9fdbd3f9a79dce2dffcad61b2725d4
Ardiy <you@example.com>
2023-06-12 14:06:33 +0000

    lists test

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,5 +4,4 @@ def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list):
         return None
     else:
-        num = my_list.pop(idx)
-        print("Element at index {:d} is {}".format(idx, num))
+        return my_list.pop(idx)
--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,5 +1,28 @@
 """task 0
+
+
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{:d}".format(i))
+        
      def main():
     my_list = [1,2,3,4,5]
     print_list_integer(my_list)
+main() """
+
+
+""" task 1
+
+def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list):
+        return None
+    else:
+        num = my_list.pop(idx)
+        print("Element at index {:d} is {}".format(idx, num))
+
+
+def main():
+    my_list = [1, 2, 3, 4, 5]
+    idx = (4)
+    element_at(my_list, idx)
 main() """
