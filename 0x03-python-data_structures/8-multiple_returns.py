b63a2dc6d676bbbbd2cf5fe328d31d11bce747b5
Ardiy <you@example.com>
2023-06-12 18:42:12 +0000

    multiple re

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,7 @@ def multiple_returns(sentence):
     length = len(sentence)
     first_char = sentence[0]
     if length == 0:
-        first_char == None
+        first_char == none
     else:
         return(length, first_char)
 
