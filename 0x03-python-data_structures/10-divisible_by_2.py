eb33f9d8e93d12bb41b6cdeef529d880f67ebd71
Ardiy <you@example.com>
2023-06-12 23:30:25 +0000

    divisible by 2 bolean appending

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+
+def divisible_by_2(my_list=[]):
+    divisible_list = []
+    for i in my_list:
+        if i % 2 == 0:
+            divisible_list.append(True)
+        else:
+            divisible_list.append(False)
+    return divisible_list
