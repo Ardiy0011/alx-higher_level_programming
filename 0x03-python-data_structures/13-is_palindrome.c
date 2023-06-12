578348eb7f629df7931d6e4fa585c00caf80b3ed
Ardiy <you@example.com>
2023-06-12 16:29:39 +0000

    print reverse

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,3 +9,20 @@ j = len (1) - 1;
 for (i = 0; i < j; i++);
 
 if (a[i] != (j- 1)
+
+
+
+
+listint_t *curr, *prev = NULL, *next;
+
+	curr = head;
+
+	while (curr)
+	{
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+
+	return (prev);
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return
     my_list.reverse()
     for i in my_list:
-        if my_list is None:
-            return
-        else:
-            print("{:d}".format(i))
+        print("{:d}".format(i))
+
