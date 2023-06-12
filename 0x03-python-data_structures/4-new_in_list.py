4545ca06c78f338f33221b36fa5512cb7f959708
Ardiy <you@example.com>
2023-06-12 17:07:59 +0000

    print reverse

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+#!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list
-    modifiedlist = [x for x in my_list]
-    modifiedlist[idx] = element
-    return newlist
+    tmp_list = my_list[:]
+    if 0 <= idx < len(my_list):
+        tmp_list[idx] = element
+        return(tmp_list)
+    return(my_list)
