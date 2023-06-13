15a0a2407fe5b7912d3e173ca97093363b4736a5
Ardiy <you@example.com>
2023-06-13 01:18:38 +0000

    laast try

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
 
-from calculator_1 import add, sub, mul, div
-import sys
-
-def calculate(a, operator, b):
-    if operator == '+':
-        return add(a, b)
-    elif operator == '-':
-        return sub(a, b)
-    elif operator == '*':
-        return mul(a, b)
-    elif operator == '/':
-        return div(a, b)
-    else:
-        return None
-
 if __name__ == "__main__":
-    length = len(sys.argv)
-    if length != 4:
+    """
+    Handle basic arithmetic operations.
+    """
+    import sys
+
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
@@ -25,9 +14,16 @@ if __name__ == "__main__":
     operator = sys.argv[2]
     b = int(sys.argv[3])
 
-    result = calculate(a, operator, b)
-    if result is None:
+    if operator == "+":
+        result = a + b
+    elif operator == "-":
+        result = a - b
+    elif operator == "*":
+        result = a * b
+    elif operator == "/":
+        result = a / b
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
+    print("{} {} {} = {}".format(a, operator, b, result))
