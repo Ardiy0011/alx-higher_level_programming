077787493565311db697f05b1e50f1acf993390a
Ardiy <you@example.com>
2023-06-13 01:05:03 +0000

    python.h

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,20 @@
+#include <Python.h>
+#include <stdio.h>
+
+void print_python_list_info(PyObject *p)
+{
+    Py_ssize_t size, i;
+    PyObject *item, *type;
+
+    size = PyList_Size(p);
+    printf("[*] Size of the Python List = %zd\n", size);
+
+    printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
+
+    for (i = 0; i < size; i++)
+    {
+        item = PyList_GetItem(p, i);
+        type = Py_TYPE(item);
+        printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
+    }
+}
