af0aade83bd650f483fdccf9bd7eb6525140e45f
Ardiy <you@example.com>
2023-06-13 00:07:21 +0000

    delete without pop

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    new_list = [x for x in my_list]
+    if idx < 0 or idx >= len(new_list):
+        return my_list
+    else:
+        new_list.remove(new_list[idx])
+        my_list.remove(my_list[idx])
+    return new_list
+
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
