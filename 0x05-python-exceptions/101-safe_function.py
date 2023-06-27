f9054ecb67ad7e452e68248d340721025cc4d55e
Ardiy <you@example.com>
2023-06-27 02:40:12 +0000

    raise name error

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -8,5 +8,5 @@ def safe_function(fct, *args):
         return fct(*args)
     except (ZeroDivisionError, ValueError, TypeError, IndexError) as er:
         print("Exception: {}".format(er), file=sys.stderr)
-    finally:
+    else:
         return None
