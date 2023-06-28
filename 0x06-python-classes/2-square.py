c3d94ddd26cce31ae506f646a9549366cd595b2f
Ardiy <you@example.com>
2023-06-28 01:37:37 +0000

    test dry run

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
+"""Define a class Square."""
 
 
 class Square:
-    """Square class with a private attribute
-    size.
-
-    """
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """Initializes the size variable as a private
-        instance artribute
+        """Initialize a new Square.
 
         Args:
-            __size (int): The __size of the new square.
-
+            size (int): The size of the new square.
         """
-        self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
+
