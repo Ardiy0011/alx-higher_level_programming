fefdf4cb6e987b0982b5d645d03f00a0da7b5f4f
Ardiy <you@example.com>
2023-06-28 00:36:35 +0000

    set size to private attribute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+"""Define a class Square."""
+class Square:
+    """initializing empty square class"""
+    def __init__(self, size):
+        """set size to private atrribute """
+        self.__size = size
