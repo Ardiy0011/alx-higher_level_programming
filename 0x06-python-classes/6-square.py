7acf104a22417e6c077df24c7357adbf67952408
Ardiy <you@example.com>
2023-06-28 16:52:19 +0000

    bug fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,7 +11,14 @@ class Square:
         Args:
             size (int): The size of the new square.
         """
+        if size != float(size):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        self.position = position
 
+    @property
     def size(self):
         """py getter"""
         return self.__size
