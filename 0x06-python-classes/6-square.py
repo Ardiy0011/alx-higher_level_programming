70108a86cb855f92457a084f52c16e7c4bdc3755
Ardiy <you@example.com>
2023-06-28 02:59:13 +0000

    print out square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,1 +1,78 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialization Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if size != float(size):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        self.position =position
+
+    @property
+    def size(self):
+        """py getter"""
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """py setter"""
+        if value != float(value):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    
+
+    @property
+    def position(self):
+        """Get/set the current position of the square."""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """area of  square."""
+        return int(self.__size) * int(self.__size)
+    
+    def my_print(self):
+        """print empty line if zero"""
+        if self.size == 0:
+            print()
+        for _ in range(self.size):
+            """printing hash square based on obj size"""
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
+
+
+my_square_1 = Square(3)
+my_square_1.my_print()
+
+print("--")
+
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
+
+print("--")
+
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
