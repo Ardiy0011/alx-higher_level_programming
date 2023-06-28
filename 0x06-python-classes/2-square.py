bd02c9de72e2c0742f20f7eb22264740a36a2197
Ardiy <you@example.com>
2023-06-28 01:32:32 +0000

    test dry run

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,17 +4,9 @@
 
 class Square:
     def __init__(self, size=0):
-        self.size = size
-
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, vva):
-        if not isinstance(vva, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif vva < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = vva
+            self.__size = size
