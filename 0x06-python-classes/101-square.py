2c88d99e5dbc1263cf204e9544e56c562bae13bc
Ardiy <you@example.com>
2023-06-28 17:00:32 +0000

    bug fix

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -67,7 +67,9 @@ class Square:
         """Define the print() representation of a Square."""
         if self.__size is not 0:
             [print("") for i in range(0, self.__position[1])]
-            for _ in range(self.__position[1]):
-                        print()
-                        for _ in range(self.__size):
-                            print(" " * self.__position[0] + "#" * self.__size)
+        for i in range(self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i is not self.__size - 1:
+                print("")
+        return ("")
