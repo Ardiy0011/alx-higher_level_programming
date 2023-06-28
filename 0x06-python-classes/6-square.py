0ee57568e532861d5d17048ab5322736a277962f
Ardiy <you@example.com>
2023-06-28 16:54:02 +0000

    bug fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -26,7 +26,7 @@ class Square:
     @size.setter
     def size(self, value):
         """py setter"""
-        if not isinstance(value, int):
+        if value != float(value):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
