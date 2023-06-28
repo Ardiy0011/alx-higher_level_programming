56ba6da29fd19cf2f566ccf8c41c187166dc22fc
Ardiy <you@example.com>
2023-06-28 02:15:01 +0000

    property adn set

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,48 @@
+
+
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialization Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if size != int(size):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        if value != int(value):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    
+    def area(self):
+        """area of  square."""
+        return int(self.__size) * int(self.__size)
+
+my_square = Square(89)
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+my_square.size = 3
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+try:
+    my_square.size = "5 feet"
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+except Exception as e:
+    print(e)
