7b84033ecd0c4653dcaedd6e583a1b871d60b070
Ardiy <you@example.com>
2023-07-04 11:32:43 +0000

    bug fixes and indentation fixes

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -53,10 +53,12 @@ class Rectangle:
         return ((self.__width * 2) + (self.__height * 2))  
 
     def __str__(self):
-        """Return the printable representation of the Rectangle using #.
+        """Return the printable representation of the Rectangle.
+        Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
             return ""
 
         rows = ['#' * self.__width for _ in range(self.__height)]
         return "\n".join(rows)
+
--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -57,14 +57,10 @@ class Rectangle:
         Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
-            return ("")
+            return ""
 
-        rect = []
-        for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
-            if i != self.__height - 1:
-                rect.append("\n")
-        return ("".join(rect))
+        rows = ['#' * self.__width for _ in range(self.__height)]
+        return "\n".join(rows)
 
 
     def __repr__(self):
