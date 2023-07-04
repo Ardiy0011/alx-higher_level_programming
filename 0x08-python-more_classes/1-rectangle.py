7c707817de379324d965537153a50d19459de2f9
Ardiy <you@example.com>
2023-07-04 03:28:29 +0000

    bug fixes and indentation fixes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,43 +1,41 @@
 #!/usr/bin/python3
-"""Defines a class rectangle."""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
-    """initializing empty rectangle class"""
+    """Represent a rectangle."""
+
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        """Initialize a new Rectangle.
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """retrieves an attribute from innit method"""
+        """Get/set the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """sets an attribute value from getter method"""
-        try:
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            elif value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
-        except (TypeError, ValueError) as e:
-            print(e)
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
-        """retrieves an attribute from innit method"""
+        """Get/set the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """sets an attribute value from getter method"""
-        try:
-            if not isinstance(value, int):
-                raise TypeError("height must be an integer")
-            elif value < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = value
-        except (TypeError, ValueError) as e:
-            print(e)
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
