7299d3fb791d51c83e51a7372238a8f403b6ad4b
Ardiy <you@example.com>
2023-07-04 02:11:15 +0000

    bug fixes and indentation fixes

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""Define a class rectangle."""
+"""
+Define a class rectangle.
+"""
 
 class Rectangle:
-    """initializing empty rectangle class"""
+    """
+    initializing empty rectangle class
+    """
     def __init__(self):
         pass
