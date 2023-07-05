0a24470fe7aebcee9835a20a4c7bac602633c6c7
Ardiy <you@example.com>
2023-07-05 21:36:18 +0000

    second bug fix

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -25,14 +25,34 @@ def text_indentation(text):
         if not text.strip():
             raise ValueError("text cannot be an empty string")
 
-        length = len(text)
-        for i in range(length):
-            print(text[i], end="")
-            if text[i] in ['.', '?', ':']:
-                text = text[i+1:].lstrip()  # Remove trailing whitespace
-                print("\n")
+        paragraphs = []
+        paragraph = ""
+
+        for char in text:
+            paragraph += char
+            if char in ['.', '?', ':']:
+                paragraphs.append(paragraph.strip())
+                paragraph = " "
+
+        if paragraph.strip():
+            paragraphs.append(paragraph.strip())
+
+        for i, paragraph in enumerate(paragraphs):
+            print(paragraph)
+            if i < len(paragraphs) - 1:
+                print()
 
     except TypeError as e:
         print(e)
     except ValueError as e:
         print(e)
+
+text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+beatiorem! Iam ruinas videres""")
