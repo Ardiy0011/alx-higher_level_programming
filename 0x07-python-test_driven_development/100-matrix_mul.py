bde44388548febc19a4fbd87bc1d5383f0d73421
Ardiy <you@example.com>
2023-07-05 23:23:34 +0000

    second bug fix

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -28,9 +28,9 @@ def matrix_mul(m_a, m_b):
         if not isinstance(m_b, list):
             raise TypeError("m_b must be a list")
 
-        if not any(isinstance(row, list) for row in m_a):
+        if not all(isinstance(row, list) for row in m_a):
             raise TypeError("m_a must be a list of lists")
-        if not any(isinstance(row, list) for row in m_b):
+        if not all(isinstance(row, list) for row in m_b):
             raise TypeError("m_b must be a list of lists")
 
         if not all(isinstance(num, (int, float)) for row in m_a for num in row):
