bc6a650eb8368563d59d737e62a766ff4c550350
Ardiy <you@example.com>
2023-07-05 19:47:04 +0000

    bug fixes

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -22,9 +22,6 @@ def text_indentation(text):
         if not isinstance (text, str):
             raise TypeError("text must be a string")
 
-        if not text.strip():
-            raise ValueError("text cannot be an empty string")
-
         paragraphs = []
         paragraph = ""
 
