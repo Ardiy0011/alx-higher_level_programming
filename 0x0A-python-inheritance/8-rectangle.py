d462500ded666b5073333026c00d08592d1195c4
Ardiy <you@example.com>
2023-07-11 13:57:00 +0000

    review bugs

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,11 +1,36 @@
 #!/usr/bin/python3
-"""
-Derived class for BaseGeometry.
-"""
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Reprsent base geometry."""
+
+    def area(self):
+        """Not yet implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate a parameter as an integer.
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        if not isinstance(name, str):
+            raise TypeError("place a string instead of {}".format(name))
 
 
 class Rectangle(BaseGeometry):
-    """derived class of base class BaseGeometry"""
+    """
+    derived class of base class BaseGeometry
+    """
+
 
     def __init__(self, width, height):
         self.__width = width
