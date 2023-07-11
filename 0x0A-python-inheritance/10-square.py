bb14f06922b799f0834cea3dcd4a03d5c339439e
Ardiy <you@example.com>
2023-07-11 14:19:17 +0000

    review bugs

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,36 +4,17 @@ class with validators
 """
 
 
-BaseGeometry = __import__('9-rectangle').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Rectangle(BaseGeometry):
-    """
-    derived class of base class BaseGeometry
-    """
-
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-
-    def area(self):
-        return(self.__width) * (self.__height)
-
-
-    def __str__(self):
-        """ print """
-        return ("[{}] {}/{}".format(__class__.__name__, self.__width, self.__height))
+"""
+the Square class
+"""
 
 
 class Square(Rectangle):
-    """
-    derived class of base class rectangle
-    """
-
+    """ Square Class """
     def __init__(self, size):
+        """ instantiation with size """
         self.__size = size
-
         super().__init__(self.__size, self.__size)
