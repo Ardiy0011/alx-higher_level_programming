df4a3cf22996933dbac3c00765d3d40d5077c067
Ardiy <you@example.com>
2023-07-11 14:12:39 +0000

    review bugs

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,32 +1,10 @@
 #!/usr/bin/python3
-"""class with validators"""
+"""
+class with validators
+"""
 
 
-class BaseGeometry:
-
-
-    def integer_validator(self, name, value):
-        """
-        function that prints the sorted list from the base class
-
-        Raises:
-        TypeError: If value not int an name not str
-        valueError: if value equal to or less than 0
-        TypeError: if empty
-  
-
-        """
-        self.name = name
-        self.value = value
-
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(self.name))
-        if isinstance(value, bool):
-            raise TypeError("{} must be an integer".format(self.name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(self.name))
-        if not isinstance(name, str):
-            raise TypeError("place a string instead of {}".format(self.name))
+BaseGeometry = __import__('9-rectangle').Rectangle
 
 
 class Rectangle(BaseGeometry):
@@ -51,6 +29,10 @@ class Rectangle(BaseGeometry):
 
 
 class Square(Rectangle):
+    """
+    derived class of base class rectangle
+    """
+
     def __init__(self, size):
         self.__size = size
 
