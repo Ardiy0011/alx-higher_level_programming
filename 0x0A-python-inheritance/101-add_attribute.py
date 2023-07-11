6f5884ab119cec85ebbc928ba0966d2c6a6a15fa
Ardiy <you@example.com>
2023-07-11 14:37:24 +0000

    review bugs

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -2,15 +2,13 @@
 """function that adds attributes to objects."""
 
 
-def add_attribute(obj, att, value):
+def add_attribute(obj, attribute, value):
     """Add a new attribute to an object if possible.
 
         TypeError: If the attribute cannot be added.
     """
-    try:
-        if not hasattr(obj, "__dict__"):
-            raise TypeError("can't add new attribute")
-        setattr(obj, att, value)
 
-    except TypeError as e:
-        print(e)
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attribute, value)
+
