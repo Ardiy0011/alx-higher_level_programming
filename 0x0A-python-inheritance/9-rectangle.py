55ce943fc7a98d59d1b4f9a8a00d7408c70e9b37
Ardiy <you@example.com>
2023-07-11 14:06:07 +0000

    review bugs

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,31 +2,8 @@
 """class with validators"""
 
 
-class BaseGeometry:
+BaseGeometry = __import__('8-rectangle').Rectangle
 
-
-    def integer_validator(self, name, value):
-        """
-        function that prints the sorted list from the base class
-
-        Raises:
-        TypeError: If value not int an name not str
-        valueError: if value equal to or less than 0
-        TypeError: if empty
-
-
-        """
-        self.name = name
-        self.value = value
-
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(self.name))
-        if isinstance(value, bool):
-            raise TypeError("{} must be an integer".format(self.name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(self.name))
-        if not isinstance(name, str):
-            raise TypeError("place a string instead of {}".format(self.name))
 
 class Rectangle(BaseGeometry):
     """derived class of base class BaseGeometry"""
