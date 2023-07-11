62aa14cfe2200ead151e8d77fc51149926d3930a
Ardiy <you@example.com>
2023-07-11 13:39:26 +0000

    review bugs

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -22,3 +22,5 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+        if not isinstance(name, str):
+            raise TypeError("place a string instead of {}".format(self.name))
