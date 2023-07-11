d875e0283bfdf50fe82811987ede05f0e2577bda
Ardiy <you@example.com>
2023-07-11 14:28:11 +0000

    review bugs

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""class with validators"""
+"""
+more class base
+"""
 
 
-BaseGeometry = __import__('10-square').Square
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 """
-the Square class
+Square class
 """
 
 
 class Square(Rectangle):
     """ Square Class """
     def __init__(self, size):
-        """ instantiation with size """
+        """ size init"""
         self.__size = size
         super().__init__(self.__size, self.__size)
 
-
     def __str__(self):
-        """ print """
-        return ("[{}] {}/{}".format(__class__.__name__, self.__size, self.__size))
+        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
