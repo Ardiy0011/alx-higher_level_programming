9d00ad6ec43d9f128ec186d6aa04c49ce9888ae3
Ardiy <you@example.com>
2023-07-11 14:14:41 +0000

    review bugs

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -2,30 +2,7 @@
 """class with validators"""
 
 
-class BaseGeometry:
-
-    def integer_validator(self, name, value):
-        """
-        function that prints the sorted list from the base class
-
-        Raises:
-        TypeError: If value not int an name not str
-        valueError: if value equal to or less than 0
-        TypeError: if empty
-  
-
-        """
-        self.name = name
-        self.value = value
-
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(self.name))
-        if isinstance(value, bool):
-            raise TypeError("{} must be an integer".format(self.name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(self.name))
-        if not isinstance(name, str):
-            raise TypeError("place a string instead of {}".format(self.name))
+BaseGeometry = __import__('10-square').Square
 
 class Rectangle(BaseGeometry):
     """
