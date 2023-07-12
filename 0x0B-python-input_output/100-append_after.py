989a7e0fa1ced0ce970fce776560e8f55a5744b3
Ardiy <you@example.com>
2023-07-12 18:55:41 +0000

    try append

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,18 +1,24 @@
+#!/usr/bin/python3
+"""
+function that readslines and writes to the next line  a anew string
+"""
+
+
 def append_after(filename="", search_string="", new_string=""):
     """
-    function that appends a new string unto a line after reading the peruse
-    in a UTF-8 text file.
+    function that appends a new string unto a line after reading the
+    peruse in a UTF-8 text file.
     """
 
     with open(filename, 'r', encoding='UTF-8') as file:
         peruse = file.readlines()
-    
-    appended = False
+
+    search_string = False
 
     with open(filename, 'w', encoding='UTF-8') as file:
         for line in peruse:
             file.write(line)
-            if '\n' in line and not appended:
+            if '\n' in line and not search_string:
                 file.write(new_string)
 
         print("success")
