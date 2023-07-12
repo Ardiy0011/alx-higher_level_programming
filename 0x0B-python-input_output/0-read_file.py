5f1a7985f7a8e1dfc5729fad5c122eb7f43c321a
Ardiy <you@example.com>
2023-07-12 02:36:01 +0000

    readme fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,6 @@ def read_file(filename="my_file_0.txt"):
     """
     function that opens and reads a file
     """
-    with open('my_file_0.txt', 'r', encoding='UTF-8') as q:
+    with open(filename, 'r', encoding='UTF-8') as q:
         Peruse = q.read()
-        print(Peruse)
+        print(Peruse, end="")
