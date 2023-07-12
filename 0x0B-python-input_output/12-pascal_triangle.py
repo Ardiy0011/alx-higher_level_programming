0764b68eff3471eb732b9667454c7789002b89b9
Ardiy <you@example.com>
2023-07-12 02:52:37 +0000

    readme fix

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -13,27 +13,3 @@ def pascal_triangle(n):
             for j in range(1, i):
                 row[j] = prev_row[j - 1] + prev_row[j]
         triangle.append(row)
-
-
-    for row in triangle:
-        print(row)
-
-
-# Test the function
-pascal_triangle(5)
-
-
-"""
-12-main
-"""
-pascal_triangle = __import__('12-pascal_triangle').pascal_triangle
-
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
-
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
