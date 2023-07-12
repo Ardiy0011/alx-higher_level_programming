7ad3be9087df22332e280ebb07e79306c9d2f02a
Ardiy <you@example.com>
2023-07-12 13:36:05 +0000

    readme fix

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,7 +9,7 @@ def save_to_json_file(my_obj, filename):
     """function that writes a json formatted string on to a text file"""
     jasonrep = json.dumps(my_obj)
 
-    with open(filename) as q:
+    with open(filename, 'w') as q:
         peruse = q.write(jasonrep)
 
     return (peruse)
