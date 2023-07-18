d9b76638e28ca09c584c741883b4713ff1310a28
Ardiy <you@example.com>
2023-07-18 03:00:31 +0000

    bug fixes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -146,6 +146,8 @@ class Rectangle(Base):
                 raise ValueError(f"{placeholder} must be >= 0")
         if value <= 0:
             raise ValueError(f"{placeholder} must be > 0")
+        if len(value) == 0:
+            raise TypeError(f"{placeholder} must be an integer")
 
 
 
