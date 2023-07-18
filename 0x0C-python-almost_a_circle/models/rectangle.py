a870a1cac06e464515e96028719fedc8afe7dd8a
Ardiy <you@example.com>
2023-07-18 02:48:52 +0000

    bug fixes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,7 +13,20 @@ class Rectangle(Base):
 
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """constructor assigmnent of each placeholder"""
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
         self.width = width
         self.height = height
         self.x = x
@@ -126,10 +139,7 @@ class Rectangle(Base):
     @staticmethod
     def setter_validation(placeholder, value):
 
-        if type(value) is not int:
-            raise TypeError(f"{placeholder} must be an integer")
-        if placeholder == "x" or placeholder == "y":
-            if value < 0:
-                raise ValueError(f"{placeholder} must be >= 0")
+        if type(value) != int:
+            raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError(f"{placeholder} must be > 0")
+            raise ValueError("width must be > 0")
