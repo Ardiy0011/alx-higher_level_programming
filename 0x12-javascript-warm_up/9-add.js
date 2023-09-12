c3222afc8455e2cac0d227155e731f22b227e5ef
Ardiy <you@example.com>
2023-09-12 01:07:22 +0000

    javascript warm up

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
 const { argv } = require('process');
-
 const first_phrase = parseInt(argv[2]);
 const second_phrase = parseInt(argv[3]);
 
 function add (a, b) {
   console.log(a + b);
 }
-add(first_phrase, second_phrase);
+  add(first_phrase, second_phrase);
