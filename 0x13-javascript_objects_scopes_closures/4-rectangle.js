0ac126cca2abcd9ea5e8b19843b3dd4eae8c5c76
Ardiy <you@example.com>
2023-09-12 23:45:05 +0000

    last 11

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-class Rectangle {
+module.exports = class Rectangle {
     constructor (w, h) {
       if (w > 0 && h > 0) {
         this.width = w;
@@ -28,5 +28,3 @@ class Rectangle {
       this.width *= 2;
     }
   };
-
-module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 counter = 0;
 exports.logMe = function (item) {
   console.log(`${counter++}: ${item}`);
