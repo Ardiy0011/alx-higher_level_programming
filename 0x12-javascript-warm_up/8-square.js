02a5c4f7e43a3fd0fe244cacc79f1b120b2d3c18
Ardiy <you@example.com>
2023-09-12 00:52:31 +0000

    javascript warm up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
-
 const { argv } = require('process');
 const x = argv[2];
 
 if (isNaN(x)) {
   console.log('Missing size');
 } else {
-
-for (let i = 1; i <= x; i++) {
-  for (let j = 0; j < x; j++) {
-    process.stdout.write('x');
+  for (let i = 0; i < x; i++) {
+    let msg = '';
+    for (let j = 0; j < x; j++) {
+      msg = msg + 'X';
+    }
+    console.log(msg);
   }
-  console.log();
-}
 }
