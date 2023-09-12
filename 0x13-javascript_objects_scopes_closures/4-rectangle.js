5d131183a922fbb97526af31d4b35cac629fda52
Ardiy <you@example.com>
2023-09-12 20:27:40 +0000

    first 6

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+class Rectangle {
+    constructor (w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      };
+    };
+
+    print () {
+      for (let i = 0; i < this.height; i++) {
+        let msg = '';
+        for (let j = 0; j < this.width; j++) {
+          msg = msg + 'X';
+        }
+        console.log(msg);
+      };
+    };
+
+    rotate () {
+      const temp = this.width;
+      this.width = this.height;
+      this.height = temp;
+    };
+
+    double () {
+      this.height *= 2;
+      this.width *= 2;
+    }
+  };
