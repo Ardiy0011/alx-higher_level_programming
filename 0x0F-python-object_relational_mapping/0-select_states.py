645cb48834eabcf6c9590ac2728c6f8cdbbbb582
Ardiy <you@example.com>
2023-09-16 19:51:17 +0000

    first five

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,41 +1,21 @@
 #!/usr/bin/python3
 
-import MySQLdb
-import sys
 
-"""
+import MySQLdb
+from sys import argv
+
+'''
 a script that lists all states
 from the database
-"""
+'''
 if __name__ == "__main__":
-    param_args = sys.argv
-
-    """connect to existing database"""
-    if len(sys.argv) != 4:
-        print("Invalid argument number")
-        sys.exit(1)
-
-    try:
-        link = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=param_args[1],
-            password=param_args[2],
-            database=param_args[3])
-
-        """use the cursor to interact with the database"""
-        cursor = link.cursor()
-
-        """query the database for what you want"""
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-        states = cursor.fetchall()
-        for eachstate in states:
-            print(eachstate)
-
-    except MySQLdb.Error as error:
-        print("Could not connect to server")
-
-        """close the database and cursor"""
-    finally:
-        cursor.close()
-        link.close()
+    con = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1],
+        password=argv[2], database=argv[3])
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    db = cursor.fetchall()
+    for i in db:
+        print(i)
+    cursor.close()
+    db.close()
