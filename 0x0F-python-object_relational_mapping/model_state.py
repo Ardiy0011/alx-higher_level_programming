7352d8fa5fb61394c9d826ddbcc6bd24bc2ee419
Ardiy <you@example.com>
2023-09-17 00:34:38 +0000

    fix indents pycode style

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,11 +4,12 @@ class to connect to databasd and name
 """
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
-
 Base = declarative_base()
 
+
 class User(Base):
+    """derive class of base declarative"""
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, unique=True, autoincrement =True, nullable=False)
+    id = Column(Integer, primary_key=True, unique=True,
+                autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
