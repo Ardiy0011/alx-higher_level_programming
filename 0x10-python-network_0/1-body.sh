81d97944c4523dac0a567c3cf7ae432a983322d7
Ardiy <you@example.com>
2023-09-29 20:42:30 +0000

    fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 # script that takes in a URL, sends GET request and displays body of response
-curl -sL "$1"
+url="$1"
+
+# Send a GET request and store the response in a temporary file
+response_file=$(mktemp)
+curl -s -o "$response_file" "$url"
+
+# Check the HTTP status code of the response
+http_status=$(curl -s -o /dev/null -w "%{http_code}" "$url")
