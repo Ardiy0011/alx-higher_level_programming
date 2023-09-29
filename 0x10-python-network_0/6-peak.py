67112582e1d7d840e0e2f0d0700956182db7eb23
Ardiy <you@example.com>
2023-09-29 11:19:12 +0000

    curl 1 fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,13 +7,13 @@ def find_peak(numbr):
     '''
         Finds the peak in a list of unsorted numbers
     '''
-    length = len(numbr)  # Change the variable name from 'len' to 'length' to avoid conflicts with the built-in 'len' function
+    length = len(numbr)
     if length == 0:
         return None
     if length == 1:
         return numbr[0]
     if length == 2:
-        return max(numbr[0], numbr[1])  # Use 'max' to return the greater of the two elements
+        return max(numbr[0], numbr[1])
 
     for index in range(length):
         value = numbr[index]
@@ -24,4 +24,4 @@ def find_peak(numbr):
             return value
         elif index == length - 1 and numbr[index - 1] <= value:
             return value
-    return None  # Change 'pick' to 'None' to handle the case when no peak is found
+    return None
