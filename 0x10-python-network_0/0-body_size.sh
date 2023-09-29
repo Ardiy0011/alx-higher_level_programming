7527054fd15801579ac6b55e3509cd221d417cd6
Ardiy <you@example.com>
2023-09-29 19:47:42 +0000

    fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-# takes in a URL and sends a request a that  displays the size of the body of the response
-curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
+# takes in a URL and sends a request that displays the size of the body of the response
+url="$1"
+response_file=$(mktemp)
+curl -sI "$url" -o "$response_file"
+size=$(wc -c < "$response_file")
+
+echo "$size"
