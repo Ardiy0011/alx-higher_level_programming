367c76a4de8b3d4bba2b681cde179dbfba7901fd
Ardiy <you@example.com>
2023-09-30 02:58:05 +0000

    pycode style

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,18 +2,13 @@
 """
 Function that sends an HTTP request and retrieves the X-Request-Id variable
 """
-import urllib.request
 import sys
-
-url = sys.argv[1]
-
-
-def get_x_request_id(url):
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
-        x_request_id = response.getheader('X-Request-Id')
-        if x_request_id:
-            print(x_request_id)
+import urllib.request
 
 
-get_x_request_id(url)
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as resp:
+        print(dict(resp.headers).get("X-Request-Id"))
