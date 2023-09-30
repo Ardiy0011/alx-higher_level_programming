fba54b8ec7fb811c411d47bd8d40085c14756ec2
Ardiy <you@example.com>
2023-09-30 02:31:28 +0000

    pycode style

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
+""" 
+Script that takes in a URL, send a request to URL, and dispaly body
 """
-Script that takes a URL, sends a request to URL, and displays the body
-"""
+
+
 if __name__ == "__main__":
     import sys
     from urllib import request, error
 
     try:
-        with request.urlopen(sys.argv[1]) as response:
-            response_encoding = response.headers.get_content_charset('utf-8')
-            response_body = response.read().decode(response_encoding)
-            print(response_body)
+        with request.urlopen(sys.argv[1]) as resp:
+            print(resp.read().decode('UTF-8'))
     except error.HTTPError as er:
         print('Error code:', er.code)
