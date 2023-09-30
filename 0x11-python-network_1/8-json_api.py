e85d527595828b6272b496b0fa51e9970791148c
Ardiy <you@example.com>
2023-09-30 02:39:32 +0000

    pycode style

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Python script that takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter
+http://0.0.0.0:5000/search_user with the letter as a parameters
 """
 import requests
 from sys import argv
